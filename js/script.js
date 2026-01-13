6cb3bb63182359908702966ef524c44ab7910a47
Duhny797 <dukhnya2u-2023@idguonline.net>
2026-01-13 16:57:05 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+}
+
+function nextSlide() {
+    currentSlide++;
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+    showSlide(currentSlide);
+}
+
+/* Попередній слайд */
+function prevSlide() {
+    currentSlide--;
+    if (currentSlide < 0) {
+        currentSlide = slides.length - 1;
+    }
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000);
